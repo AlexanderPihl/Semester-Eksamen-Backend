aa916db9bc6e5324eecb70b74d6ee52da59e2d34
Alexander Pihl <cph-as509@cphbusiness.dk>
2020-11-13 17:01:51 +0100

    Quotes from Breaking Bad

--- src/main/java/rest/ApplicationConfig.java
+++ src/main/java/rest/ApplicationConfig.java
@@ -23,6 +23,7 @@ public class ApplicationConfig extends Application {
         resources.add(cors.CorsFilter.class);
         resources.add(errorhandling.GenericExceptionMapper.class);
         resources.add(org.glassfish.jersey.server.wadl.internal.WadlResource.class);
+        resources.add(rest.Breaking_BadResource.class);
         resources.add(rest.JokeResource.class);
         resources.add(rest.ScrapeResource.class);
         resources.add(rest.UserResource.class);
